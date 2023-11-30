c2ee6ab9ffb8d05ad32e3e3a2fe2e93512abf2a2
Atraski <145670772+Atraski@users.noreply.github.com>
2023-11-30 12:38:13 +0530

    Update HomeComponents.js

--- src/components/HomeComponents.js
+++ src/components/HomeComponents.js
@@ -1,5 +1,7 @@
-import React,{useState} from 'react';
+import React,{useState , useEffect, useRef} from 'react';
 import {Link} from 'react-router-dom';
+import logo from '../images/atstaylogo.webp';
+
 import Carousel from 'react-bootstrap/Carousel';
 import carousel1 from '../images/carouselimg1.webp';
 import carousel2 from '../images/carouselimg2.webp';
@@ -15,13 +17,18 @@ import camping from '../images/camping.jpg';
 import trending from '../images/trending.jpg';
 import mansion from '../images/Mansion.jpg';
 import tea from '../images/tea.jpg';
+import less from '../images/less.png';
 // import './HomeComponents.css';
 import Footer from './Footer';
+import './HomeComponents.css';
+import { useSwipeable } from 'react-swipeable';
+import Dropdown from './dropdown';
 
 import { productData } from './Atstaysdata';
 
 export default function HomeComponents() {
   const [imgs, setImg] = useState(productData);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const btnpressprev = () => {
     // const box = document.getElementById('box'); // Get the element by its id
@@ -35,6 +42,8 @@ export default function HomeComponents() {
       console.error('Element with id "box" not found.');
     }
   }
+
+  
   
   const btnpressnext = () => {
     // const box = document.getElementById('box'); // Get the element by its id
@@ -55,26 +64,219 @@ export default function HomeComponents() {
       // Filter product cards based on the clicked image's id
       
       const filteredImgs = imgs.filter((img) => img.ids === productId);
-      console.log(filteredImgs);
-      setImg(filteredImgs);
-     
-      
-      
-      
+      // const filteredImgs1 = imgs.filter((img) => img.ids1 === productId2);
+      console.log(filteredImgs)
+        setImg(filteredImgs);
+        // setImg(filteredImgs1);
+
+      // Update the search query with the selected destination
+      setSearchQuery(filteredImgs[0].place);    
     };
 
+
+    const handleProductClick1 = (productId) => {
+      // Filter product cards based on the clicked image's id
+      
+      const filteredImgs = imgs.filter((img) => img.ids1 === productId);
+      // const filteredImgs1 = imgs.filter((img) => img.ids1 === productId2);
+      console.log(filteredImgs)
+        setImg(filteredImgs);
+        // setImg(filteredImgs1);
+
+      // Update the search query with the selected destination
+      setSearchQuery(filteredImgs[0].place);    
+    };
+
+    const handleProductClick2 = (productId) => {
+      // Filter product cards based on the clicked image's id
+      
+      const filteredImgs = imgs.filter((img) => img.ids2 === productId);
+      // const filteredImgs1 = imgs.filter((img) => img.ids1 === productId2);
+      console.log(filteredImgs)
+        setImg(filteredImgs);
+        // setImg(filteredImgs1);
+
+      // Update the search query with the selected destination
+      setSearchQuery(filteredImgs[0].place);    
+    };
+
+    const handleProductClick3 = (productId) => {
+      // Filter product cards based on the clicked image's id
+      
+      const filteredImgs = imgs.filter((img) => img.ids3 === productId);
+      // const filteredImgs1 = imgs.filter((img) => img.ids1 === productId2);
+      console.log(filteredImgs)
+        setImg(filteredImgs);
+        // setImg(filteredImgs1);
+
+      // Update the search query with the selected destination
+      setSearchQuery(filteredImgs[0].place);    
+    };
+
+
+    const handleProductClick4 = (productId) => {
+      // Filter product cards based on the clicked image's id
+      
+      const filteredImgs = imgs.filter((img) => img.ids4 === productId);
+      // const filteredImgs1 = imgs.filter((img) => img.ids1 === productId2);
+      console.log(filteredImgs)
+        setImg(filteredImgs);
+        // setImg(filteredImgs1);
+
+      // Update the search query with the selected destination
+      setSearchQuery(filteredImgs[0].place);    
+    };
+
+    const handleProductClick5 = (productId) => {
+      // Filter product cards based on the clicked image's id
+      
+      const filteredImgs = imgs.filter((img) => img.ids5 === productId);
+      // const filteredImgs1 = imgs.filter((img) => img.ids1 === productId2);
+      console.log(filteredImgs)
+        setImg(filteredImgs);
+        // setImg(filteredImgs1);
+
+      // Update the search query with the selected destination
+      setSearchQuery(filteredImgs[0].place);    
+    };
+
+    const handleProductClick6 = (productId) => {
+      // Filter product cards based on the clicked image's id
+      
+      const filteredImgs = imgs.filter((img) => img.ids6 === productId);
+      // const filteredImgs1 = imgs.filter((img) => img.ids1 === productId2);
+      console.log(filteredImgs)
+        setImg(filteredImgs);
+        // setImg(filteredImgs1);
+
+      // Update the search query with the selected destination
+      setSearchQuery(filteredImgs[0].place);    
+    };
+
+
+    const handleProductClick7 = (productId) => {
+      // Filter product cards based on the clicked image's id
+      
+      const filteredImgs = imgs.filter((img) => img.ids7 === productId);
+      // const filteredImgs1 = imgs.filter((img) => img.ids1 === productId2);
+      console.log(filteredImgs)
+        setImg(filteredImgs);
+        // setImg(filteredImgs1);
+
+      // Update the search query with the selected destination
+      setSearchQuery(filteredImgs[0].place);    
+    };
+
+    const handleSwipe = (direction) => {
+      console.log(`Swiped ${direction}`);
+      if (direction === 'left') {
+        btnpressnext();
+      } else if (direction === 'right') {
+        btnpressprev();
+      }
+    };
   
+    // Use the useSwipeable hook to add swipe functionality
+    const handlers = useSwipeable({
+      onSwipedLeft: () => handleSwipe('left'),
+      onSwipedRight: () => handleSwipe('right'),
+    });
   
+
+    const [isCoverVisible, setIsCoverVisible] = useState(false);
+    const coverRef = useRef(null);
+    const anywhereRef = useRef(null);
+
+    const openbox = () => {
+        setIsCoverVisible(!isCoverVisible);
+    };
+
+    const handleOutsideClick = (event) => {
+        if (coverRef.current && !coverRef.current.contains(event.target) && !anywhereRef.current.contains(event.target)) {
+            setIsCoverVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('click', handleOutsideClick);
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, []);
+    const filteredProducts = imgs.filter((img) =>
+    img.place.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   //  
 
     return (
       <>
-        <div className="product-carousel">
+     <div style={{ margin: 'auto' }}>
+            <div className="boxrad my-3">
+                <div className="subbox">
+                    <div className="any" ref={anywhereRef} onClick={openbox}>AnyWhere</div>
+                </div>
+            </div>
 
-<button className="pre-btn" onClick={btnpressprev}><p>&lt;</p></button>
-<button className="next-btn" onClick={btnpressnext}><p>&gt;</p></button>
+            <div className="coverss">
+                <div
+                    className={`cover ${isCoverVisible ? 'visible' : ''}`}
+                    ref={coverRef}
+                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                >
+                    <nav className="navbar navbar-expand-xl navbar-light" style={{ width: '668px' }}>
+                        <div className="imap" style={{display:'flex' , justifyContent:'space-around' , width:'100%' , alignItems:'center'}}>
+                            <Link to="/" className="navbar-brand">
+                                <img src={logo} alt="Logo" className="logoimg" />
+                            </Link>
+        
 
-<div className="product-container">
+        <div className="smtp" id="" style={{width:'300px'}}>
+          <ul className="navbar-nav falja">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/attours" className="nav-link">
+                Tour
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/atstays" className="nav-link"> 
+                Stays
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/bloggerpage" className="nav-link">
+                Blogs
+              </Link>
+            </li>
+          </ul>
+        </div>
+        
+        <div>
+            <input type="text" className="desti" placeholder="Destination" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} style={{maxWidth:"150px" ,height:'0px' , marginTop:'13px' , padding:'15px' , border:'1px solid #000' , borderRadius:'20px'}}/>
+            {/* <i class="fa-solid fa-magnifying-glass mx-"></i> */}
+        </div>
+      </div>
+
+      
+      
+    </nav>
+
+            </div>
+        </div>
+
+
+    </div>
+
+        <div className="product-carousel" {...handlers}>
+
+<button className="pre-btn" onClick={btnpressprev}><p><img src={less} style={{width:'22px'}}/></p></button>
+<button className="next-btn" onClick={btnpressnext}><p><img src={less} style={{width:'22px',transform: 'scaleX(-1)'}}/></p></button>
+
+<div className="product-container" >
   <div>
 <img
   className="d-block mycard"
@@ -91,11 +293,11 @@ export default function HomeComponents() {
   className="d-block mycard"
   src={beach}
   alt="First slide"
-  onClick={() => handleProductClick(2)} 
+  onClick={() => handleProductClick1(2)} 
   
   
 />
-<p>Beach</p>
+<p>River</p>
 </div>
 
 <div>
@@ -103,11 +305,11 @@ export default function HomeComponents() {
   className="d-block mycard"
   src={lake}
   alt="First slide"
-  onClick={() => handleProductClick(3)} 
+  onClick={() => handleProductClick2(3)} 
   
   
 />
-<p>Lakes</p>
+<p>View</p>
 </div>
 
 <div>
@@ -115,10 +317,10 @@ export default function HomeComponents() {
   className="d-block mycard"
   src={trending}
   alt="First slide"
-  onClick={() => handleProductClick(4)} 
+  onClick={() => handleProductClick3(4)} 
   
 />
-<p>Trending</p>
+<p>Trek</p>
 </div>
 
 <div>
@@ -126,7 +328,7 @@ export default function HomeComponents() {
   className="d-block mycard"
   src={camping}
   alt="First slide"
-  
+  onClick={() => handleProductClick4(5)}
   
 />
 <p>Camping</p>
@@ -137,10 +339,10 @@ export default function HomeComponents() {
   className="d-block mycard"
   src={mansion}
   alt="First slide"
-  
+  onClick={() => handleProductClick5(6)}
   
 />
-<p>Mansion</p>
+<p>SnowFall</p>
 </div>
 
 <div>
@@ -148,10 +350,10 @@ export default function HomeComponents() {
   className="d-block mycard"
   src={tea}
   alt="First slide"
-  
+  onClick={() => handleProductClick6(7)}
   
 />
-<p> Breakfast</p>
+<p> Mountains</p>
 </div>
 
 <div>
@@ -159,35 +361,15 @@ export default function HomeComponents() {
   className="d-block mycard"
   src={rooms}
   alt="First slide"
-  
-  
-/>
-<p>rooms</p>
-</div>
-
-<div>
-<img
-  className="d-block mycard"
-  src={rooms}
-  alt="First slide"
-  
+  onClick={() => handleProductClick7(8)}
   
 />
-<p>rooms</p>
+<p>Amazing View</p>
 </div>
 
-<div>
-<img
-  className="d-block mycard"
-  src={rooms}
-  alt="First slide"
-  
-  
-/>
-<p>rooms</p>
-</div>
 
-<div>
+
+{/* <div>
 <img
   className="d-block mycard"
   src={rooms}
@@ -232,31 +414,30 @@ export default function HomeComponents() {
   
 />
 <p>rooms</p>
-</div>
-</div>
+</div>*/}
+</div> 
 
 
 
 </div>  
 
-<div className="container " style={{display:'flex' , flexWrap:'wrap',gap:'50px' , justifyContent:'center'}}>
-{
-  imgs.map((elm)=>{
+<div className="container-fluid" style={{display:'flex' , flexWrap:'wrap',gap:'50px' , justifyContent:'center'}}>
+{imgs.map((elm) => {
     return(
       <>
-      <div className=" " style={{display:'flex' }}>
+      <div className="" style={{display:'flex' }}>
       <Link to={`/atstay/${elm.id}`}>
-        <div className="card mt-5" style={{ borderRadius: '15px', width: '280px', height: '320px', border:'none' }}>
+        <div className="card mt-5 cds mb-5" style={{ borderRadius: '15px', width: '300px', height: '320px', border:'none' }}>
                         <div className="images">
                           <img
                             src={elm.imges}
                             className="img-fluid"
-                            style={{ height: '250px', width: '280px', borderRadius:'15px' }}
+                            style={{ height: '262px', width: '300px', borderRadius:'15px' }}
                           ></img>
                         </div>
 
                         <div className="tourinfo">
-                          <p className="my-2 mx-2" style={{ textTransform: 'uppercase', fontSize: '13px' , fontWeight:'700' }}>
+                          <p className="my-2 mx-2 d-flex" style={{ textTransform: 'uppercase', fontSize: '13px' , fontWeight:'700' }}>
                             <i className="fa-solid fa-location-dot mx-1"></i>
                             {elm.place}
                           </p>
